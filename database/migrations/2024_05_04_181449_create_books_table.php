6b06ea37d507b802d47e6112dc8b78e986bc248b
mArchilli <archillimatias@gmail.com>
2024-05-04 15:45:42 -0300

    book migration

---
+++ database/migrations/2024_05_04_181449_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('editorial')->nullable();
+            $table->unsignedSmallInteger('anio_publicacion');
+            $table->string('isbn')->unique();
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+};
