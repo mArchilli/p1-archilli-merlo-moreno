93931cd9f981f7be9eb34a61714e0ede9bbe1528
mArchilli <archillimatias@gmail.com>
2024-05-10 21:51:56 -0300

    vista blog diseño corregido

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,27 +5,31 @@
 <x-layout>
 
 <x-slot:title>Blog</x-slot:title>
-
-@foreach ($blog as $posteo)
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-    <img class="img-fluid w-50" src="{{ $posteo->imagen }}" alt="Imagen del libro">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ $posteo->titulo }}</h5>
-        <p class="card-text">{{ $posteo->subtitulo }}</p>
-        <p class="card-text">{{ $posteo->categoria }}</p>
-        <p class="card-text">{{ $posteo->texto }}</p>
-        <p class="card-text">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </p>
+<div class="row ">
+  @foreach ($blog as $posteo)
+  <div class="col-12 col-md-6">
+    <div class="card mb-3 cards-blog justify-content-between">
+      <div class="row g-0 flex-column ">
+        <div class="col-12">
+        <img class="img-fluid" src="{{ $posteo->imagen }}" alt="Imagen del libro">
+        </div>
+        <div class="col-12">
+          <div class="card-body">
+            <h5 class="card-title">{{ $posteo->titulo }}</h5>
+            <p class="card-text">{{ $posteo->subtitulo }}</p>
+            <p class="card-text">{{ $posteo->categoria }}</p>
+            <p class="card-text">{{ $posteo->texto }}</p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
   @endforeach
 </div>
+
 
 <!-- otra opción: https://codepen.io/cristinaconacel/pen/dgmjKK -->
 </x-layout>
